fd65c55b9b2dc72feb2120393352072f08008ac0
zuciopath <73157609+zuciopath@users.noreply.github.com>
2022-11-03 12:36:01 +0400

    .

---
+++ app.js
@@ -0,0 +1,20 @@
+const name = 'zurab';
+
+function toUpper(text){
+const uppercased = text.toUpperCase();
+console.log(uppercased);
+}
+
+toUpper(name);
+/*
+I created a function in the following 
+code that I used on a variable called 
+,,name'' to make it uppercase and gave 
+value to the text to do so.
+*/
+
+const schedule = ['get up','move','turn left','move','die'];
+console.log(schedule);
+schedule.pop(); //removes the last element.
+schedule.push('die'); //adds specified element to the end of the array.
+schedule.reverse(); //array has been reversed.
